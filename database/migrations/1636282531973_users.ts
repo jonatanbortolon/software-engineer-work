cdfdccffd92cb15a54e2e1856671ee8ac189bb2b
Jonatan Gabriel Bortolon <jonatangabrielbortolon@outlook.com>
2021-11-29 13:28:09 -0300

    Minor fixes

--- database/migrations/1636282531973_users.ts
+++ database/migrations/1636282531973_users.ts
@@ -14,7 +14,7 @@ export default class UsersSchema extends BaseSchema {
         .enum('role', ['ADMIN', 'SALESMAN', 'STOCKIST'], {
           useNative: true,
           enumName: 'user_account_role',
-          existingType: true,
+          existingType: false,
         })
         .defaultTo('ADMIN')
       table.string('phone').nullable()
